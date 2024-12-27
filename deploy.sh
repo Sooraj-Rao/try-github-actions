b07a95608aea396df2d8a45fd1dcaec3a2941919
Sooraj-Rao <soorajrao630@gmail.com>
2024-12-27 20:32:36 +0530

    add

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Navigate to the application directory
-cd ~/app
+# Define the app directory path
+APP_DIR="/home/ubuntu/nextjs"
 
-# Pull the latest code from the repository
-git pull origin main
-
-# Build and run the Docker container
-docker build -t demo .
+# Stop any existing containers
 docker stop demo || true
 docker rm demo || true
-docker run  -d --name demo -p 3001:3001  demo
+
+# Check if the repository is already cloned
+if [ -d "$APP_DIR" ]; then
+    echo "Repository already exists. Pulling the latest changes..."
+    cd $APP_DIR
+    git pull origin main
+else
+    echo "Repository not found. Cloning the repository..."
+    git clone https://github.com/Sooraj-Rao/demo-vite-react.git $APP_DIR
+    cd $APP_DIR
+fi
+
+# Build the Docker image
+docker build -t demo .
+
+# Run the Docker container
+docker run -d -p 3001:3001 demo
+
+# Optionally, you can check the logs to ensure everything is running properly
+docker logs -f demo
